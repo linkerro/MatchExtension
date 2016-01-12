b232d5c58b4416cc1d07a45877822534bbbe6767
Radu Iscu <r.iscu@levi9.com>
2016-01-12 19:18:56 +0200

    Added support for basic matching

---
+++ MatchExtension/MatchExtension.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchExtensions
+{
+    public static class MatchExtension
+    {
+        public static void Match<T>(T operand, List<KeyValuePair<  Func<T, bool>,Action<T>>> matches)
+        {
+            foreach (var match in matches)
+            {
+                var condition = match.Key;
+                var code = match.Value;
+                if (condition(operand))
+                {
+                    code(operand);
+                    break;
+                }
+            }
+        }
+
+        public static void Match<T>(T operand, IEnumerable<Case<T>> matches)
+        {
+            foreach (var match in matches)
+            {
+                if (match.Condition(operand))
+                {
+                    match.Code(operand);
+                    break;
+                }
+            }
+        }
+
+        public static Case<T> Case<T>(Func<T, bool> condition, Action<T> code)
+        {
+            return new Case<T> {Condition = condition,Code = code};
+        }
+
+        public static Default<T> Default<T>(Action<T> code)
+        {
+            return new Default<T> {Code = code};
+        }
+    }
+
+    public class Case<T>
+    {
+        public Func<T,bool> Condition { get; set; }
+        public Action<T> Code { get; set; }
+    }
+
+    public class Default<T>:Case<T>
+    {
+        public Default()
+        {
+            Condition = x => true;
+        }  
+    }
+}
---
+++ MatchExtensionTests/MatchExtensionTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using MatchExtensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static MatchExtensions.MatchExtension;
+
+namespace MatchExtensionTests
+{
+    [TestClass]
+    public class MatchExtensionTests
+    {
+        [TestMethod]
+        public void SimpleMatchesShouldWork()
+        {
+            var result = "no result";
+            Match(1,new List<Case<int>>
+            {
+                Case<int>(x=>x==1,x=>result="we found the one"),
+                Default<int>(_=>result="the default is now "+_)
+            });
+            Assert.AreEqual("we found the one",result);
+        }
+
+        [TestMethod]
+        public void DefaultMatchShouldWork()
+        {
+            var result = "no result";
+            Match(345, new List<Case<int>>
+            {
+                Case<int>(x=>x==1,x=>result="we found the one"),
+                Default<int>(_=>result="the default is now "+_)
+            });
+            Assert.AreEqual("the default is now 345", result);
+        }
+    }
+}
